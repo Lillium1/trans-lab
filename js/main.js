f5d8d347360292bf4d46d3a776f3a2d65041ca0c
Paulina Gonzalez C <pgc.lacroix@gmail.com>
2017-07-14 09:38:45 -0400

    se agrega la funcionalidad de side-nav

--- js/main.js
+++ js/main.js
@@ -10,12 +10,10 @@ $(document).ready(function(){
 	$("#email").blur(function() {
 		if(!(/^([a-z]+[a-z1-9._-]*)@{1}([a-z1-9\.]{2,})\.([a-z]{2,3})$/.test($("#email").val()))){
 			$(this).css("border-color", "red");
-			$("label").attr("data-errores", "*Ingresa un correo válido");
+			$(".l-email").attr("data-error", "*Ingresa un correo válido");
 			$("#email").val("");
 			EmailError = true;
-		}
-
-		else {
+		} else {
 			$(this).css("border-color", "green");
 			localStorage.setItem('email',$("#email").val());
 			EmailError = false;
@@ -24,29 +22,35 @@ $(document).ready(function(){
 
 	/*----------* PASSWORD *----------*/
 	$("#password").blur(function() {
-		if (!(/^\d*$/.test($("#password").val())) || $(this).val()=='') {
+		if (!(/^\d{1,8}([0-9])*$/.test($(this).val()))) {
 			$(this).css("border-color", "red");
-			$("label").attr("data-error", "*Debe ingresar sólo numeros");
+			$(".l-pass").attr("data-error", "*Debe una contraseña válida");
 			$("#password").val("");
 			PasswordError = true;
-		}
-
-		else {
+		} else {
 			$(this).css("border-color", "green");
+			localStorage.setItem('password',$("#password").val());
 			PasswordError = false;
 		}
 	})
-
+	/*----------* DESACTIVAR/ACTIVAR EL BOTON NEXT SI CAMPOS NO VACIOS *----------*/
 	$('button[type="submit"]').attr('disabled','disabled');
-	$("#email,#password").click(function() {  
-        if($(this).val()!=''){ 
-            $('button[type="submit"]').removeAttr('disabled');  
-        } else {  
-        	$('button[type="submit"]').attr('disabled','disabled');  
+	$("input").blur(function() {  
+		event.preventDefault();
+        if($("#email").val()!='' && $("#password").val()!=''){ 
+            $('button[type="submit"]').removeAttr('disabled');
+        }
+        else {  
+        	$('button[type="submit"]').attr('disabled','disabled');
         }  
     });
-    
-	/*----------* COMPROBAR QUE LOS CAMPOS ESTEN CORRECTOS *----------*/
+    /*----------* IR A LA SIGUEINTE PAGINA *----------*/
+    $('button[type="submit"]').click(function() {
+    	event.preventDefault();
+        window.location.href="index_home.html";
+    }); 
+   
+	/*----------* COMPROBAR  *----------*/
 	$(".contact-form").submit(function(event) {
 		if(EmailError === true || PasswordError === true){
 		 	event.preventDefault(); // prevent sending 
@@ -54,7 +58,12 @@ $(document).ready(function(){
 	 	}
 	});
 
+
+	/********************************** HOME ***********************************/
+
+  	$('.button-collapse').sideNav();
+
+
 });
 
 
-/********************************** HOME ***********************************/
